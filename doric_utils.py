544fe4a29ec73a25fc25ab56ea3f4cd4663982f0
tstevenson <tstevenson@ucdavis.edu>
2024-05-25 15:08:44 -0700

    Added ability to print all attributes and values in a doric file. Updated the function to fill in missing data to be more robust

--- doric_utils.py
+++ doric_utils.py
@@ -17,19 +17,26 @@ def ish5dataset(item):
     return isinstance(item, h5py.Dataset)
 
 
-def h5printR(item, leading = ''):
+def h5printR(item, leading = '', print_attr=False):
     for key in item:
         if ish5dataset(item[key]):
             print(leading + key + ': ' + str(item[key].shape))
+            if print_attr:
+                for a in item[key].attrs:
+                    print(' ' + leading + a + ': ' + repr(item[key].attrs[a]))
         else:
             print(leading + key)
-            h5printR(item[key], leading + '  ')
+            if print_attr:
+                for a in item[key].attrs:
+                    print(' ' + leading + a + ': ' + repr(item[key].attrs[a]))
+            h5printR(item[key], leading + '  ', print_attr)
+
 
 # Print structure of a .doric file
-def h5print(filename):
+def h5print(filename, print_attr=False):
     with h5py.File(filename, 'r') as h:
         print(filename)
-        h5printR(h, '  ')
+        h5printR(h, '  ', print_attr)
 
 
 def h5read(filename, where):
@@ -61,8 +68,9 @@ def h5getDatasetR(item, leading = ''):
         firstkey = list(item[key].keys())[0]
         if ish5dataset(item[key][firstkey]):
             r = r+[{'Name':leading+'_'+key, 'Data':
-                                                [{'Name': k, 'Data': np.array(item[key][k]),
-                                                    'DataInfo': {atrib: item[key][k].attrs[atrib] for atrib in item[key][k].attrs}} for k in item[key]]}]
+                     [{'Name': k,
+                       'Data': np.array(item[key][k]),
+                       'DataInfo': {atrib: item[key][k].attrs[atrib] for atrib in item[key][k].attrs}} for k in item[key]]}]
         else:
             r = r+h5getDatasetR(item[key], leading + '_' + key)
 
@@ -129,19 +137,25 @@ def fill_missing_data(flat_data, time_key = 'time'):
 
     issues = []
     #skipped_idxs = {}
+    ts_skipped_thresh = 0.01
 
     # check timstamps for any skipped information
     for name in flat_data.keys():
         time = flat_data[name][time_key]
         ts_diffs = np.diff(time)
-        close = np.isclose(mode(ts_diffs)[0], ts_diffs, atol=2e-6, rtol=0)
+
+        # find the timestep difference that is closest to the rest of the differences
+        unique_diffs = np.unique(np.round(ts_diffs, 8))
+        diff_close_counts = [sum(np.isclose(d, ts_diffs, atol=2e-6, rtol=0)) for d in unique_diffs]
+        best_diff = unique_diffs[np.argmax(diff_close_counts)]
+        close = np.isclose(best_diff, ts_diffs, atol=2e-6, rtol=0)
+        dt = np.round(np.mean(ts_diffs[close]), 8)
 
         if not all(close):
-            dt = np.round(np.mean(ts_diffs[close]), 8)
             idxs = np.where(~close)[0]+1
             # round to nearest decimal because timestamps can be off by a multiple of a step, based on the decimation
             ts_skipped = utils.convert_to_multiple(ts_diffs[~close]/dt, 0.001)-1
-            
+
             #skipped_idxs[name] = {'idxs': idxs, 'skipped steps': ts_skipped}
 
             signal_names = [k for k in flat_data[name].keys() if k != time_key]
@@ -152,14 +166,16 @@ def fill_missing_data(flat_data, time_key = 'time'):
             for idx, n_skip in zip(idxs, ts_skipped):
 
                 # if we skipped one timestep or less, this is because of a bug with writing data to file
-                # where the data is written at the next cycle, but the timestamp still increments, 
+                # where the data is written at the next cycle, but the timestamp still increments,
                 # so we need to reduce the rest of the timestamps by the skipped amount
-                if n_skip <= 1 :
-                    for sig_name in signal_names:
+                if n_skip <= 1:
+                    if abs(n_skip) < ts_skipped_thresh:
+                        continue
+                    else:
                         time[idx+idx_offset:] -= dt*n_skip
 
                 else: # we skipped multiple steps, so need to insert nans
-                
+
                     # see whether there were any partial steps skipped as well
                     sub_skips = n_skip % 1
                     full_skips = int(n_skip)
@@ -169,29 +185,32 @@ def fill_missing_data(flat_data, time_key = 'time'):
                         flat_data[name][sig_name] = np.insert(flat_data[name][sig_name],
                                                              idx+idx_offset,
                                                              np.full(full_skips, np.nan))
-                    
+
                     # update time array
                     time = np.insert(time, idx+idx_offset,
                                      time[idx+idx_offset-1] + dt*np.arange(1, full_skips+1))
-                    
+
                     idx_offset += full_skips
-                    
+
                     # account for partial skips
-                    if sub_skips != 0:
+                    if sub_skips != 0 and abs(sub_skips) > ts_skipped_thresh:
                         time[idx+idx_offset:] -= dt*sub_skips
-
-            # make sure time array is evenly spaced by the dt
-            # account for any initial offset of the first point that is less than dt
-            offset = time[0] % dt
-            flat_data[name][time_key] = utils.convert_to_multiple(time-offset, dt) + offset
-
-            if np.sum(~np.isclose(dt, np.diff(flat_data[name][time_key]), atol=2e-6, rtol=0)) != 0:
-                raise ValueError('Missing data in signal {} could not be filled properly'.format(name))
 
             # log issues
             skip_counts = Counter(ts_skipped)
             issues.append('Signal {0} had {1} total time skips:\n{2}'.format(
                 name, np.sum(ts_skipped),
                 '\n'.join(['  {0} step(s): {1} time(s)'.format(k,v) for k,v in skip_counts.items()])))
+
+        # make sure time array is evenly spaced by the dt
+        # account for any initial offset of the first point that is less than dt
+        offset = time[0] % dt
+        time = utils.convert_to_multiple(time-offset, dt) + offset
+
+        if np.sum(~np.isclose(dt, np.diff(time), atol=2e-6, rtol=0)) != 0:
+            raise ValueError('Time array for signal {} could not be evenly spaced by dt: {}'.format(name, dt))
+
+        flat_data[name][time_key] = time
+
 
     return flat_data, issues
